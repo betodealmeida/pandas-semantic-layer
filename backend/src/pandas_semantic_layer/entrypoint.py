e2730099a809e5f7dd79b6d1fb68ae7a2035f234
Beto Dealmeida <roberto@dealmeida.net>
2026-02-09 14:59:25 -0500

    Use temporary registry

--- backend/src/pandas_semantic_layer/entrypoint.py
+++ backend/src/pandas_semantic_layer/entrypoint.py
@@ -1,1 +1,5 @@
+from pandas_semantic_layer.semantic_layer import PandasSemanticLayer
+from superset.semantic_layers.registry import registry
+
+registry["pandas"] = PandasSemanticLayer
 print("Pandas Semantic Layer extension registered")
