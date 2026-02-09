0cfb2afe31bcda005a778d6378a3f6536b80961c
Beto Dealmeida <roberto@dealmeida.net>
2026-02-09 13:27:25 -0500

    Fix paths

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,7 +3,7 @@ import { core } from "@apache-superset/core";
 
 export const activate = (context: core.ExtensionContext) => {
   context.disposables.push(
-    core.registerViewProvider("pandas.example", () => <p>Pandas Semantic Layer</p>)
+    core.registerViewProvider("pandas_semantic_layer.example", () => <p>Pandas Semantic Layer</p>)
   );
   console.log("Pandas Semantic Layer extension activated");
 };
