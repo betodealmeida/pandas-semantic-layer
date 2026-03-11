ffe913fc92b8a7fcd4ade8bc56c00c9a406d0606
Beto Dealmeida <roberto@dealmeida.net>
2026-03-11 13:56:23 -0400

    Fixes for new release

--- tmp/backend/src/pandas_semantic_layer/__init__.py
+++
@@ -1,9 +0,0 @@
-from pandas_semantic_layer.schemas import PandasConfiguration
-from pandas_semantic_layer.semantic_layer import PandasSemanticLayer
-from pandas_semantic_layer.semantic_view import PandasSemanticView
-
-__all__ = [
-    "PandasConfiguration",
-    "PandasSemanticLayer",
-    "PandasSemanticView",
-]
--- tmp/backend/src/pandas_semantic_layer/entrypoint.py
+++
@@ -1,6 +0,0 @@
-from superset.semantic_layers.registry import registry
-
-from .semantic_layer import PandasSemanticLayer
-
-registry["pandas"] = PandasSemanticLayer
-print("Pandas Semantic Layer extension registered")
